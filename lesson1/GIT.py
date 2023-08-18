002dee200ca6bd22a8c0b4bdfea0c9e8d18b5a4e
Alexey <allexis1110@gmail.com>
2023-08-18 18:59:33 +0300

    Алгоритм

---
+++ lesson1/GIT.py
@@ -0,0 +1,20 @@
+#def strcounter(s):
+#    for sym in s:
+#        counter = 0
+#        for sub_sym in s:
+#            if sym == sub_sym:
+#                counter += 1
+#        print(sym, counter)
+
+#strcounter('abca')
+
+
+def strcounter(s):
+    for sym in set(s):
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter += 1
+        print(sym, counter)
+
+strcounter('abca')
